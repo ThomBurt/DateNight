6a4aba8729953834708b7c50d1f877d2e61a2fb1
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-01-25 13:00:30 +0000

    updating pop ups

--- js/create-account.js
+++ js/create-account.js
@@ -14,7 +14,6 @@
 
                         //create account
                         var createAccountDivEl = document.getElementById('create-account-div');
-
                         var createAccountEl = document.getElementById('create-account-btn');
                         createAccountEl.addEventListener('click', createAccountButton);
 
@@ -29,9 +28,14 @@
 
                         function closeCreateAccountPopUp() {
                             createAccountDivEl.classList.add("display-none");
+                            console.log('clicked');
                         }
                         
 
+                        // 1st right close out button
+                        var blackXbuttonRight = document.getElementById('x-btn-right-1').addEventListener('click', closeCreateAccountPopUp);
+                        // 2nd page left close out button
+                        var leftXButton = document.getElementById('x-btn-left-1').addEventListener('click', closeCreateAccountPopUp);
 
 
                         // http://devingine.com/post/how-to-make-login-and-registration-form-html-css-and-javascript-with-source-code
